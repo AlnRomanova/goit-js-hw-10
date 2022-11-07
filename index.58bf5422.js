6533cda7b5328c028bad86ca85f39f146e99c84d
AlnRomanova <111745365+AlnRomanova@users.noreply.github.com>
2022-11-07 21:38:31 +0000

    Deploying to gh-pages from @ AlnRomanova/goit-js-hw-10@3e6fbcf232a2df281cff1f304af244b2ca2bc2f9 🚀

---
+++ index.58bf5422.js
@@ -0,0 +1,2 @@
+({fetchCountries:function(e){return fetch("${BASE_URL}/name/${name}").then((e=>e.json()))}}).fetchCountries();
+//# sourceMappingURL=index.58bf5422.js.map
---
+++ index.a8ffdbd0.js
@@ -0,0 +1,2 @@
+fetch("${BASE_URL}/name/${name}").then((function(n){return n.json()}));
+//# sourceMappingURL=index.a8ffdbd0.js.map

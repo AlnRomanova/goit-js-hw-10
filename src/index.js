3e6fbcf232a2df281cff1f304af244b2ca2bc2f9
Alina Romanova <alinakhraban@gmail.com>
2022-11-07 23:38:05 +0200

    Add edits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import API from './fetchCountries.js'
 
 const DEBOUNCE_DELAY = 300;
+
+API.fetchCountries()
+
+
+// fetch('https://restcountries.com/v2/name/{name}')
+// .then(response => {
+//  console.log(response.json());
+// })
+// .then(country => {
+//   console.log(country)
+// })

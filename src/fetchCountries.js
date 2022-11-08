736130c58e4bce512a99734b88aeb89caf4ae4ed
Alina Romanova <alinakhraban@gmail.com>
2022-11-08 20:08:02 +0200

    Add edits

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v2'
 
 function fetchCountries(name) {
- return fetch(`${BASE_URL}/name/${name}?fields=name,capital,currencies`).then(response => response.json());
+ return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flag,languages`).then(response => response.json());
+ 
 }
 
 export default {fetchCountries};
--- src/index.js
+++ src/index.js
@@ -1,26 +1,66 @@
 import './css/styles.css';
 import API from './fetchCountries.js'
+import debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 
 const inputCountryName = document.querySelector('#search-box');
-inputCountryName.addEventListener('input', handleSearchCountry);
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
+inputCountryName.addEventListener('input', debounce(handleSearchCountry,DEBOUNCE_DELAY));
 
-function  handleSearchCountry (e) {
+function handleSearchCountry (e) {
 
-const searchQuery = e.target.value;
-
+const searchQuery = e.target.value.trim();
 
  API.fetchCountries(searchQuery)
-.then((countries) => console.log(countries))
-.catch((error) => console.log(error));
+.then((countries) => {
+  if(countries.length > 10) {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    return
+  }
+  renderUserList(countries)
+} )
+
+.catch((error) => Notiflix.Notify.failure("Oops, there is no country with that name"));
 
 }
 
-// function renderCountryName (name) {
-// fetch('https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages')
+function renderUserList(countries) {
+  let markup;
+  if (countries.length === 1) {
+    markup = countries
+    .map((country) => {
+      return `<li>
+          <img src="${country.flag}" width="40" heigth="40">
+          <p>${country.name}</p>
+          <p>${country.capital}</p>
+          <p>${country.population}</p>
+          <p>${country.languages.map(e => e.name).join(', ')}</p>
+        </li>`;
+    })
+    .join("");
+  }  else {
+  markup = countries
+    .map((country) => {
+      return `<li>
+          <img src="${country.flag}" width="40" heigth="40">
+          <p>${country.name}</p>
+        </li>`;
+    })
+    .join("");
+
+  }
+
+   countryListEl.innerHTML = markup;
+}
 
 
-// };
+
+
+
+
+
